319ff5817927eb1d3c0943295a5a8c91a8b45623
limu <1259567994@qq.com>
2022-09-08 17:25:09 +0800

    1

--- src/views/baseinfo/components/search.vue
+++ src/views/baseinfo/components/search.vue
@@ -4,15 +4,21 @@
       <el-row>
         <el-col :span="6">
           <p class="text">{{ searchOne }}</p>
-          <el-input v-model="inputOne" placeholder="请输入内容" />
+          <el-input v-model="inputOne" placeholder="请输入" />
         </el-col>
         <el-col :span="6">
           <p class="text">{{ searchTwo }}</p>
-          <el-input v-model="inputTwo" placeholder="请输入内容" />
+          <el-input v-model="inputTwo" placeholder="请输入" />
         </el-col>
         <el-col :span="6">
           <p class="text">{{ searchThree }}</p>
-          <el-input v-model="inputThree" placeholder="请输入内容" />
+          <el-select v-if="searchThree === '仓库状态' || '库位状态'" v-model="inputThree" placeholder="请输入" />
+          <el-input v-else v-model="inputThree" placeholder="请输入" />
+        </el-col>
+        <el-col :span="6">
+          <p class="text" style="opacity:0"> as</p>
+          <el-button type="primary" round style="color : black;background-color :#ffb200;border:unset">搜索</el-button>
+          <el-button type="primary" round style="color : black;background-color:#f8f5f5;border:unset">重置</el-button>
         </el-col>
       </el-row>
       <div />
